f8799c380bce12332757bb14dff53cf45d305374
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-02-29 10:44:24 +0000

    Array type and default values examples

--- src/pkg2darrays_17thofjanuary/Main.java
+++ src/pkg2darrays_17thofjanuary/Main.java
@@ -21,6 +21,28 @@ public class Main {
         
         int [][] JaggedArray = { {1,2,3}, {4,5}, {6,7,8,9,} }; // is possibel to have different size in each row called Jagged Arrays, not very used but posible 
     
+        int [][] NotSpecifiedSizes = new int [5][];// is possibel to only put teh fist size of teh inner array. Java will compail. But is better practise to add both dimmensions. 
+    
+        System.out.println(AnotherArray[1][0]); // index Array 1 and 0
+        
+        System.out.println(TwoDArray[1][0]); // 0 zero (int default value)
+        
+       // System.out.println(NotSpecifiedSizes[1][0]); // Second (inner) array is unallocated, so there is nothing to read
+    
+        double[] doubleArray = new double [10]; // 0.0 (double array default)
+        System.out.println(doubleArray[4]);
+        
+        
+        //create and initiliase the array 
+        boolean[] booleanArray = new boolean [10]; //default value for boolean is false which means 0 zero
+        System.out.println(booleanArray[4]);
+        
+        char[] charArray = new char[6];
+        System.out.println(charArray[3]);// default will be blank space 
+       
+        String[] stringArray = new String[10];
+        System.out.println(stringArray[4]); // default value will be null 
+                
     }
     
 }
